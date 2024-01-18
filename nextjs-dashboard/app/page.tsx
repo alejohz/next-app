83bb035b29d945f22c73165cd1bb26ceb47db8a6
Alejandro Henao <alejandrohenao91@gmail.com>
2024-01-18 12:16:39 -0500

    Formatting with prettier

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p
-            className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${montserrat.className} antialiased font-semibold`}
+            className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${montserrat.className} font-semibold antialiased`}
           >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -35,14 +35,14 @@ export default function Page() {
             src="/hero-desktop.png"
             width={1000}
             height={760}
-            className='hidden md:block'
+            className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className='block md:hidden'
+            className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
